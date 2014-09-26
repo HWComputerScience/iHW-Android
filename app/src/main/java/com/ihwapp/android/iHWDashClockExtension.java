137ea60a3d6f884ed37d4382a5f72e37c7246fef
Ethan Madison <ethan@ethanmad.com>
2014-09-26 16:35:43 -0700

    Extension shows up and (probably) hides when there is no value for secsUntil (no time until next class). Possibly shows real time values

--- app/src/main/java/com/ihwapp/android/iHWDashClockExtension.java
+++ app/src/main/java/com/ihwapp/android/iHWDashClockExtension.java
@@ -1,5 +1,6 @@
 package com.ihwapp.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
@@ -14,18 +15,32 @@ public class iHWDashClockExtension extends DashClockExtension {
 
     public static final String PREF_NAME = "pref_name";
 
+    @Override
+    protected void onInitialize(boolean isReconnect) {
+        setUpdateWhenScreenOn(true);
+    }
+
     protected void onUpdateData(int reason) {
         // Get preference value
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = sp.getString(PREF_NAME, "testing");
         // Timer time =  TODO: read time from the active countDownTimer in PeriodView or DayFragment and format it
+        long secsUntil = PeriodView.staticSecsUntil;
+        String secs = "" + secsUntil % 60;
+        if (secsUntil % 60 < 10) secs = "0" + secs;
 
         // Publish the extension data update.
         publishUpdate(new ExtensionData()
                 .visible(true)
                 .icon(R.drawable.ic_launcher)
-                .status("Hello")
-                .expandedTitle("Period starts in: " )
-                .expandedBody("This is a test extension")
-                .contentDescription("This shows the time remaining until your next class."));
+                .status(secsUntil / 60 + ":" + secs)
+                .expandedTitle("iHW")
+                .expandedBody("Next period starts in " + secsUntil / 60 + ":" + secs)
+                .contentDescription("This shows the time remaining until your next class.")
+                .clickIntent(new Intent(this, LaunchActivity.class)));
+
+        // Hide extension if there's no data.
+        if (secsUntil == 0)
+            publishUpdate(null);
     }
 }
