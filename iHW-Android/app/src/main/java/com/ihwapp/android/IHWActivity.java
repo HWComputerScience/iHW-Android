244982a28ea7bb2e5dca0656587c3c53cf83be5f
Ethan Madison <ethan@ethanmad.com>
2014-11-04 00:14:08 -0800

    Revert "Use AppCompat Theme to bring Lollipop features to all"

--- iHW-Android/app/src/main/java/com/ihwapp/android/IHWActivity.java
+++ iHW-Android/app/src/main/java/com/ihwapp/android/IHWActivity.java
@@ -1,14 +1,13 @@
 package com.ihwapp.android;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-
 import com.ihwapp.android.model.Curriculum;
 
-public class IHWActivity extends ActionBarActivity {
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Curriculum.ctx = this.getApplicationContext();
-    }
+import android.app.Activity;
+import android.os.Bundle;
+
+public class IHWActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Curriculum.ctx = this.getApplicationContext();
+	}
 }
