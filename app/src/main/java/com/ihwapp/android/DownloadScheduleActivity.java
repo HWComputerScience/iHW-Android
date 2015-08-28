c855073af5fbabd160f34f58f0a1448b73d5b77f
ile123ile <bbravencofounder@gmail.com>
2015-08-28 15:59:31 -0700

    Deleted unnecessary import

--- app/src/main/java/com/ihwapp/android/DownloadScheduleActivity.java
+++ app/src/main/java/com/ihwapp/android/DownloadScheduleActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.webkit.ConsoleMessage;
